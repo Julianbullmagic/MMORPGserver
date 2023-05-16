bc0d3d3ccf5e0ecec489bb10f6813840ea2cf9a7
Julian Bull <julianbullmagic@gmail.com>
2023-05-16 23:31:25 +1000

    ffff

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-setInterval(getState, 200)
+// setInterval(getState, 200)
 
 function getState(){
   // console.log("getting state")
